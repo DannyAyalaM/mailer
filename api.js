dff59650b8515ff48ffd009ffeac80081583965a
DannyAyalaM <ayaladanny18@gmail.com>
2022-10-20 13:50:16 -0300

    upload files

---
+++ api.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import sgMail from '@sendgrid/mail'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('app'))
+const port = process.env.PORT
+sgMail.setApiKey(process.env.SGKEY)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${path.resolve()}/index.html`)
+})
+
+app.post('/send', async (req, res) => {
+    const { to, subject, html } = req.body
+
+    const msg = {
+        to,
+        from: process.env.FROM,
+        subject,
+        text: html,
+        html: html
+    }
+
+    try {
+        await sgMail.send(msg)
+        res.sendStatus(204)
+    } catch (error) {
+        const messages = error.response.body.errors.map(e => e.message).join(' ')
+        res.status(400).send(messages)
+    }
+})
+
+app.listen(port, () => {
+    console.log('Server on PORT => ', port)
+})
---
+++ app/main.js
@@ -0,0 +1,58 @@
+loadTemplateInital = () => {
+    template = `
+        <div class="container">
+            <h1>Mailer</h1>
+            <hr>
+            <div class="img-container">
+                <img class="img-1" src="images/mailer-img.jpg">
+                <img class="img-2" id="img-2" src="images/avion-mail.png">
+            </div>
+            <form id="form">
+                <div class="email"><input type="text" name="to" placeholder="juan@perez.com"/></div>
+                <div class="asunto"><input type="text" name="subject" placeholder="Asunto"/></div>
+                <div class="text"><textarea name="text"></textarea></div>
+                <div class="send"><button id="send" type="submit">Send</button></div>
+            </form>
+            <div id="error"></div>
+        </div>
+
+    `
+    const body = document.getElementsByTagName('body')[0]
+    body.innerHTML = template
+}
+
+const enviar = () => {
+    const form = document.getElementById('form')
+    const imgMove = document.getElementById('img-2')
+    form.onsubmit = async (e) => {
+        e.preventDefault()
+        imgMove.style.animation = 'volar 10s'
+        const error = document.getElementById('error')
+        error.innerHTML = ''
+        const formData = new FormData(form)
+        const data = Object.fromEntries(formData.entries())
+        const response = await fetch('/send', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        const responseText = await response.text()
+        if (response.status > 300) {
+            
+            error.innerHTML = responseText
+            return 
+        }
+        form.reset()
+        
+        alert('Correo enviado con exito')
+        
+    }
+}
+
+window.onload = () => {
+    loadTemplateInital()
+    enviar()
+}
+
